b59e28646321f3c27fed9b9a847bbabb0d0a4379
Thang Nguyen <thang.nguyentho@digiex.asia>
2022-12-05 15:32:23 +0700

    fix bug ui

--- src/components/SettingBoxChat/index.tsx
+++ src/components/SettingBoxChat/index.tsx
@@ -9,20 +9,21 @@ function SettingBoxChat(props: Props) {
         <div
             className="w-[360px] h-[150] bg-[#fff] fixed bottom-[280px] right-[20px] rounded-lg shadow-lg shadow-white-500/50 mr-[7px]"
             style={{padding: '8px 0 8px 0'}}>
-            <div
-                className="flex items-center rounded-[6px] cursor-pointer outline-none p-[8px] text-sm hover:bg-[#e5e5e5] leading-5 font-normal"
-                style={{margin: '0 8px'}}>
-                <a target="_blank" href="https://www.facebook.com/thang.tho2003">
+            <a target="_blank" href="https://www.facebook.com/thang.tho2003">
+                <div
+                    className="flex items-center rounded-[6px] cursor-pointer outline-none p-[8px] text-sm hover:bg-[#e5e5e5] leading-5 font-normal"
+                    style={{margin: '0 8px'}}>
                     Truy cập Trang Facebook
-                </a>
-            </div>
-            <div
-                className="flex items-center rounded-[6px] cursor-pointer outline-none p-[8px] text-sm hover:bg-[#e5e5e5] leading-5 font-normal"
-                style={{margin: '0 8px'}}>
-                <a target="_blank" href="https://www.messenger.com/desktop/launch">
+                </div>
+            </a>
+            <a target="_blank" href="https://www.messenger.com/desktop/launch">
+
+                <div
+                    className="flex items-center rounded-[6px] cursor-pointer outline-none p-[8px] text-sm hover:bg-[#e5e5e5] leading-5 font-normal"
+                    style={{margin: '0 8px'}}>
                     Thêm Messenger vào trang web của bạn
-                </a>
-            </div>
+                </div>
+            </a>
         </div>
     )
 }
